246464f99fcb79dcef672033ce445d1c2779bded
daniel21connor <jconnorr1@miumg.edu.gt>
2022-11-12 08:25:58 -1100

    se estan agregando el formulario y como se trabajara con el array y su metodo Daniel connor

---
+++ src/java/Class/LibroModel.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+public class LibroModel {
+    private int code;
+    private String  nombre;
+    private String fecha;
+    private String autor;
+    private int id;
+    
+        
+    
+    public LibroModel(int code,  String nombre, String fecha, String autor, int id){
+        this.code=code;
+        this.nombre=nombre;
+        this.fecha=fecha;
+        this.autor=autor;
+        this.id=id;
+        
+        
+    }
+    
+
+}
